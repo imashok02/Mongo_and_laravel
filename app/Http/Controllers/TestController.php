5e83274340dc375c0af870e2455749f1ee2656b9
ashok <ashok@pentoz.com>
2018-02-09 15:58:45 +0530

    configuring mongodb in laravel

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $values = ['name' => 'Ashok Tester', 'email' => 'ashok@pentoz.com'];
+
+        $query = $this->connection->insert($values);
+
+        return $query;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/MongoConnection/DBConnection.php
@@ -0,0 +1,51 @@
+<?php
+namespace App;
+
+
+// require_once 'vendor/autoload.php';
+
+class DBConnection
+{
+	 const HOST = 'localhost';
+	 const PORT = 27017;
+	 const DBNAME = 'learn';
+	 private static $instance;
+	 public $connection;
+	 public $database;
+
+
+    private function __construct()
+	 {
+	 	$connectionString = sprintf('mongodb://%s:%d',
+		 DBConnection::HOST,
+		 DBConnection::PORT
+		);
+	 try 
+	 {
+		 $this->connection = new MongoDB\Client($connectionString);
+		 $this->database = $this->connection->selectDatabase(DBConnection::DBNAME);
+	 } 
+	 catch (MongoConnectionException $e)
+	 {
+	 	throw $e;
+	 }
+	}
+
+	static public function instantiate()
+	 {
+
+	 if (!isset(self::$instance)) 
+	 	{
+		 $class = __CLASS__;
+		 self::$instance = new $class;
+	 	}
+	 return self::$instance;
+	 
+	 }
+
+
+	 public function getCollection($name)
+	 {
+	 	return $this->database->selectCollection($name);
+	 }
+ }
